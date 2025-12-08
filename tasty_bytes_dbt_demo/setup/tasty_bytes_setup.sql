f59e002c1cd93ec4bb68a70cf9a83becd4605b05
Nicholas Boss <bos18002@byui.edu>
2025-12-08 21:09:45 +0000

    fixed setup script

--- tasty_bytes_dbt_demo/setup/tasty_bytes_setup.sql
+++ tasty_bytes_dbt_demo/setup/tasty_bytes_setup.sql
@@ -1,5 +1,4 @@
 USE ROLE training_role;
-
 -- CREATE OR REPLACE WAREHOUSE tasty_bytes_dbt_wh
 --     WAREHOUSE_SIZE = 'small'
 --     WAREHOUSE_TYPE = 'standard'
@@ -7,7 +6,6 @@ USE ROLE training_role;
 --     AUTO_RESUME = TRUE
 --     INITIALLY_SUSPENDED = TRUE
 --     COMMENT = 'warehouse for tasty bytes dbt demo';
-
 USE WAREHOUSE animal_task_wh;
 
 CREATE DATABASE IF NOT EXISTS tasty_bytes_dbt_db;
@@ -19,21 +17,17 @@ CREATE OR REPLACE SCHEMA tasty_bytes_dbt_db.prod;
 -- ALTER SCHEMA tasty_bytes_dbt_db.dev SET LOG_LEVEL = 'INFO';
 -- ALTER SCHEMA tasty_bytes_dbt_db.dev SET TRACE_LEVEL = 'ALWAYS';
 -- ALTER SCHEMA tasty_bytes_dbt_db.dev SET METRIC_LEVEL = 'ALL';
-
 -- ALTER SCHEMA tasty_bytes_dbt_db.prod SET LOG_LEVEL = 'INFO';
 -- ALTER SCHEMA tasty_bytes_dbt_db.prod SET TRACE_LEVEL = 'ALWAYS';
 -- ALTER SCHEMA tasty_bytes_dbt_db.prod SET METRIC_LEVEL = 'ALL';
-
 -- CREATE OR REPLACE API INTEGRATION git_integration
 --   API_PROVIDER = git_https_api
 --   API_ALLOWED_PREFIXES = ('https://github.com/')
 --   ENABLED = TRUE;
-
 -- CREATE OR REPLACE NETWORK RULE tasty_bytes_dbt_db.public.dbt_network_rule
 --   MODE = EGRESS
 --   TYPE = HOST_PORT
 --   VALUE_LIST = ('hub.getdbt.com', 'codeload.github.com');
-
 -- CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION dbt_access_integration
 --   ALLOWED_NETWORK_RULES = (tasty_bytes_dbt_db.public.dbt_network_rule)
 --   ENABLED = true;
